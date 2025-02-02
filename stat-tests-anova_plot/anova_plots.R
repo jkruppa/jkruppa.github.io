e8e2415b205ae4f08afbd346c1bd199c2b46f456
Jochen Kruppa <jkruppa@users.noreply.github.com>
2025-02-02 20:54:41 +0100

    added

--- stat-tests-anova_plot/anova_plots.R
+++ stat-tests-anova_plot/anova_plots.R
@@ -1,4 +1,5 @@
 
+## ---------------------------------------------------------------------------
 ## f statistic
 p_f_stat <- ggplot(data.frame(x = c(0, 5)), aes(x)) +
   theme_minimal() +
@@ -33,6 +34,7 @@ p_f_stat <- ggplot(data.frame(x = c(0, 5)), aes(x)) +
        title = expression(Signifikante~Teststatistik~F[D]),
        subtitle = expression(Die~berechnete~Teststatistik~F[D]~ist~größer~als~der~kritische~Wert~F[alpha*'='*5*'%']))
 
+## ---------------------------------------------------------------------------
 ## function to get data
 get_data_tbl <- function(mean = c(7, 7, 7), sd = c(2, 2, 2), ng = 13) {
   assump_tbl <- tibble(A = rnorm(ng, mean[1], sd[1]),
@@ -58,6 +60,7 @@ get_data_tbl <- function(mean = c(7, 7, 7), sd = c(2, 2, 2), ng = 13) {
              grand_mean = grand_mean))
 }
 
+## ---------------------------------------------------------------------------
 ## example ggplot
 
 fac1_tbl <- read_xlsx("data/flea_dog_cat_fox.xlsx") |> 
@@ -75,6 +78,7 @@ fac1_tbl <- fac1_tbl |>
          beta = grp_mean - grand_mean,
          epsilon = jump_length - grp_mean) 
 
+
 p_example_fac1 <- fac1_tbl |> 
   ggplot() +
   aes(pos, jump_length) +
@@ -91,29 +95,29 @@ p_example_fac1 <- fac1_tbl |>
                aes(y = jump_length), 
                xend = 1:7, yend = rep(8.13, 7), color = "gray70",
                linewidth = 0.5) +
-  geom_segment(x = 1, y = 8.13, xend = 7, yend = 8.13, color = "#E69F00", 
+  geom_segment(x = 1, y = 8.13, xend = 7, yend = 8.13, color = "#D55E00", 
                linewidth = 1) +
-  geom_segment(x = 4.5, y = grand_mean, xend = 4.5, yend = 8.13, color = "#E69F00", 
+  geom_segment(x = 4.5, y = grand_mean, xend = 4.5, yend = 8.13, color = "#D55E00", 
                linewidth = 0.5) +
   annotate("text", x = 4.75, y = grand_mean + 0.35, label = expression(beta[A.1]), size = 5,
-           color = "#E69F00", hjust = "left") +
-  annotate("text", x = -1.25, y = 8.13, label = expression(bar(y)[A.1]), size = 5,
-           color = "#E69F00", hjust = "left") +
-  geom_segment(x = 11, y = 4.74, xend = 17, yend = 4.74, color = "#56B4E9", 
+           color = "#D55E00", hjust = "left") +
+  annotate("text", x = -1.25, y = 8.13, label = expression(bar(y)[A.1]), size = 4,
+           color = "#D55E00", hjust = "left") +
+  geom_segment(x = 11, y = 4.74, xend = 17, yend = 4.74, color = "#0072B2", 
                linewidth = 1) +
-  geom_segment(x = 14.5, y = grand_mean, xend = 14.5, yend = 4.74, color = "#56B4E9", 
+  geom_segment(x = 14.5, y = grand_mean, xend = 14.5, yend = 4.74, color = "#0072B2", 
                linewidth = 0.5) +
   annotate("text", x = 12.25, y = grand_mean - 1, label = expression(beta[A.2]), size = 5,
-           color = "#56B4E9", hjust = "left") +
-  annotate("text", x = 17.5, y = 4.74, label = expression(bar(y)[A.2]), size = 5,
-           color = "#56B4E9", hjust = "left") +
+           color = "#0072B2", hjust = "left") +
+  annotate("text", x = 17.5, y = 4.74, label = expression(bar(y)[A.2]), size = 4,
+           color = "#0072B2", hjust = "left") +
   geom_segment(x = 21, y = 9.16, xend = 27, yend = 9.16, color = "#009E73",
                linewidth = 1) +
   geom_segment(x = 24.5, y = grand_mean, xend = 24.5, yend = 9.16, color = "#009E73",
                linewidth = 0.5) +
   annotate("text", x = 24.75, y = grand_mean + 0.8, label = expression(beta[A.3]), size = 5,
            color = "#009E73", hjust = "left") +
-  annotate("text", x = 18.5, y = 9.16, label = expression(bar(y)[A.3]), size = 5,
+  annotate("text", x = 18.5, y = 9.16, label = expression(bar(y)[A.3]), size = 4,
            color = "#009E73", hjust = "left") +
   geom_point() +
   geom_hline(yintercept = grand_mean, color = "gray50", linewidth = 1) +
@@ -133,6 +137,8 @@ p_example_fac1 <- fac1_tbl |>
                      expression(epsilon[16]), expression(epsilon[17]), expression(epsilon[18]),
                      expression(epsilon[19]), expression(epsilon[20]), expression(epsilon[21])),
            color = "gray20")
+
+## ---------------------------------------------------------------------------
 
 p_example_fac_1_total <- fac1_tbl |> 
   mutate(pos = rep(1:21)) |> 
@@ -154,8 +160,105 @@ p_example_fac_1_total <- fac1_tbl |>
   geom_point() +
   geom_hline(yintercept = grand_mean, color = "gray50", linewidth = 1) +
   scale_x_continuous(breaks = 11, labels = "") +
-  scale_y_continuous(breaks = grand_mean, labels = expression(bar(Y)[Total])) +  
+  scale_y_continuous(breaks = grand_mean, labels = expression(beta[0])) +  
   theme(axis.text = element_text(size = 14),
         axis.text.y = element_text(size = 16),
         axis.title.x = element_text(size = 16, face = 2)) + 
   labs(x = "", y = "")
+
+## ---------------------------------------------------------------------------
+
+get_intro_data_tbl <- function(mean = c(7, 7, 7), sd = c(2, 2, 2), ng = c(4, 3, 5)) {
+  assump_tbl <- tibble(rsp = c(rnorm(ng[1], mean[1], sd[1]),
+                               rnorm(ng[2], mean[2], sd[2]),
+                               rnorm(ng[3], mean[3], sd[3])),
+                       trt = as_factor(rep(c("A", "B", "C"), ng))) 
+  stat <- plyr::ddply(assump_tbl, "trt", summarize,
+                      mean = mean(rsp, na.rm = TRUE),
+                      sd = round(sd(rsp, na.rm = TRUE), 2),
+                      var = round(var(rsp, na.rm = TRUE), 2),
+                      max = max(mean - min(rsp), max(rsp) - mean)*1.05) |> 
+    mutate(pos = c(2, 9, 18),
+           rsp = 0)
+  grand_mean <- mean(assump_tbl$rsp, na.rm = TRUE)
+  plot_tbl <- assump_tbl |> 
+    mutate(pos = c(seq(1, ng[1], 1), 
+                   seq(from = ng[1] + 4, to = ng[1] + ng[2] + 3, by = 1), 
+                   seq(ng[1] + ng[2] + 7, ng[1] + ng[2] + ng[3] + 6, 1)),
+           grand_mean = grand_mean,
+           grp_mean = rep(stat$mean, ng),
+           beta = grp_mean - grand_mean,
+           epsilon = rsp - grp_mean) 
+  return(lst(plot = plot_tbl,
+             stat = stat,
+             grand_mean = grand_mean))
+}
+
+## ---------------------------------------------------------------------------
+
+get_ex_lst_plot <- function(ex_lst){
+  p <- ex_lst$plot |> 
+    ggplot() +
+    aes(pos, rsp) +
+    theme_minimal() +
+    geom_hline(yintercept = ex_lst$grand_mean, color = "gray75", linewidth = 1) +
+    geom_point2(color = "gray25") +
+    geom_segment(x = 1, y = ex_lst$stat$mean[1], xend = 7, yend = ex_lst$stat$mean[1], 
+                 color = "#D55E00", linewidth = 1) +
+    annotate("text", x = 7.5, y = ex_lst$stat$mean[1], label = expression(bar(y)[A.1]), 
+             size = 4, color = "#D55E00", hjust = "left") +
+    geom_segment(x = 11, y = ex_lst$stat$mean[2], xend = 17, yend = ex_lst$stat$mean[2],
+                 color = "#0072B2", linewidth = 1) +
+    annotate("text", x = 17.5, y = ex_lst$stat$mean[2], label = expression(bar(y)[A.2]), 
+             size = 4, color = "#0072B2", hjust = "left") +
+    geom_segment(x = 21, y = ex_lst$stat$mean[3], xend = 27, yend = ex_lst$stat$mean[3],
+                 color = "#009E73", linewidth = 1) +
+    annotate("text", x = 27.5, y = ex_lst$stat$mean[3], label = expression(bar(y)[A.3]), 
+             size = 4, color = "#009E73", hjust = "left") +
+    geom_errorbar(data = ex_lst$stat, aes(ymin = mean - max, ymax = mean + max), 
+                  width = 0.5, color = c("#D55E00", "#0072B2", "#009E73"), linetype = 11)  +
+    scale_x_continuous(breaks = c(4, 14, 24), labels = c("A.1", "A.2", "A.3"),
+                       limits = c(NA, 29))  +
+    scale_y_continuous(breaks = ex_lst$grand_mean, labels = expression(beta[0]),
+                       limits = c(-4, 14)) +  
+    theme(axis.text = element_text(size = 14),
+          axis.text.y = element_text(size = 16),
+          plot.title = element_text(size = 17),
+          plot.subtitle = element_text(size = 12, face = "italic")) + 
+    labs(x = "", y = "") 
+  return(p)
+}
+
+## ---------------------------------------------------------------------------
+
+get_ex_lst_intro_plot <- function(ex_lst){
+  p <- ex_lst$plot |> 
+    ggplot() +
+    aes(pos, rsp) +
+    theme_minimal() +
+    geom_hline(yintercept = ex_lst$grand_mean, color = "gray75", linewidth = 1) +
+    geom_point(color = "gray50", size = 2) +
+    geom_segment(x = 1, y = ex_lst$stat$mean[1], xend = 4, yend = ex_lst$stat$mean[1], 
+                 color = "#D55E00", linewidth = 1.5) +
+    annotate("text", x = 4.5, y = ex_lst$stat$mean[1], label = expression(bar(y)[A.1]), 
+             size = 5, color = "#D55E00", hjust = "left") +
+    geom_segment(x = 8, y = ex_lst$stat$mean[2], xend = 10, yend = ex_lst$stat$mean[2],
+                 color = "#0072B2", linewidth = 1.5) +
+    annotate("text", x = 10.5, y = ex_lst$stat$mean[2], label = expression(bar(y)[A.2]), 
+             size = 5, color = "#0072B2", hjust = "left") +
+    geom_segment(x = 14, y = ex_lst$stat$mean[3], xend = 18, yend = ex_lst$stat$mean[3],
+                 color = "#009E73", linewidth = 1.5) +
+    annotate("text", x = 18.5, y = ex_lst$stat$mean[3], label = expression(bar(y)[A.3]), 
+             size = 5, color = "#009E73", hjust = "left") +
+    scale_x_continuous(breaks = c(2, 9, 18), labels = c("A.1", "A.2", "A.3"),
+                       limits = c(NA, 20))  +
+    scale_y_continuous(breaks = ex_lst$grand_mean, labels = expression(beta[0]),
+                       limits = c(0, 10)) +  
+    theme(axis.text = element_text(size = 14),
+          axis.text.y = element_text(size = 16),
+          plot.title = element_text(size = 17),
+          plot.subtitle = element_text(size = 12, face = "italic")) + 
+    labs(x = "", y = "") 
+  return(p)
+}
+
