91b2e95377638ed88f653b2276554bc3e6b5c6a9
Jochen Kruppa <jkruppa@users.noreply.github.com>
2025-06-04 10:08:37 +0200

    added

--- images/R/stat-tests-utest.R
+++ images/R/stat-tests-utest.R
@@ -150,3 +150,111 @@ p_high_d <- tibble(rsp = c(rnorm(10000, 5, 1), rnorm(10000, 12, 1)),
         legend.text = element_text(size = 12),
         legend.title = element_text(size = 14, face = 2),
         legend.position = "none") 
+
+set.seed(20250605)
+shift_tbl <- tibble(A = c(rnorm(10, 5, 2), 7, 8, 13, 14, 14),
+                    B = A + 5) |> 
+  gather()
+
+p_shift <- shift_tbl |> 
+  ggplot(aes(x = value, fill = key)) +
+  theme_minimal() +
+  geom_density(trim = FALSE, alpha = 0.5) +
+  geom_vline(xintercept = c(5, 10), color = "black", linewidth = 0.7) +
+  geom_segment(aes(x = 5, xend = 10, y = 0.14, yend = 0.14),
+               linetype = 2, color = "gray50",
+               arrow = arrow(length = unit(0.03, "npc"), type = "closed")) +
+  annotate("label", x = 7.5, y = 0.14, label = expression(a>0),
+           size = 5) +  
+  scale_x_continuous(limits = c(-2, 24), 
+                     breaks = c(5.4, 10.4),
+                     labels = c(expression(tilde(y)[dog]),
+                                expression(tilde(y)[cat]))) +
+  scale_fill_okabeito() +  
+  scale_y_continuous(limits = c(NA, 0.15)) +
+  labs(title = "Gleiche Verteilungsform mit unterschiedlicher Lage",
+       subtitle = "Die beiden Verteilungen sind um den Wert a im Median verschoben") +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(), 
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 14),        
+        plot.title = element_text(size = 17),
+        plot.subtitle = element_text(size = 12, face = "italic"),
+        plot.caption = element_text(size = 12),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14, face = 2),
+        legend.position = "none") 
+
+
+hypo_null_tbl <- tibble(Dog = c(NA, NA, NA, 4, NA, NA, NA),
+                        Cat = c(1,2,3,5,6,7,8)) |> 
+  gather() |> 
+  na.omit() |> 
+  mutate(col = as_factor(c(1, 2, 2, 2, 3, 3, 3,3)),
+         key = fct_rev(as_factor(key)))
+
+p1_hypo <- hypo_null_tbl |> 
+  ggplot(aes(x = value, y = key, fill = col)) +
+  theme_minimal() +
+  geom_point(shape = 21, size = 5, show.legend = FALSE) +
+  scale_fill_okabeito() +
+  annotate("text", x = c(1, 2, 3, 5, 6, 7, 8)+0.2, y = 1-0.15, 
+           label = c(expression(c[1]), expression(c[2]), expression(c[3]),
+                     expression(c[4]), expression(c[5]), expression(c[6]),
+                     expression(c[7]))) +
+  annotate("text", x = c(4)+0.2, y = 2-0.15, 
+           label = expression(d[1])) +
+  annotate("label", x = 6.1, y = 2, hjust = "left",
+           label = expression(Pr~(Dog<Cat)~"="~frac(3, 7)~"≈"~0.5))+
+  labs(x = "", y = "") +
+  theme(axis.title.y = element_text(size = 14, face = 2),
+        axis.text.y = element_text(size = 12), 
+        axis.title.x = element_text(size = 14, face = 2),
+        axis.text.x = element_blank(),
+        plot.title = element_text(size = 17),
+        plot.subtitle = element_text(size = 12, face = "italic"),
+        plot.caption = element_text(size = 12),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14, face = 2),
+        legend.position = "none")
+
+hypo_alt_tbl <- tibble(Dog = c(1, NA, NA, NA, NA, NA, NA),
+                       Cat = c(2,3,4,5,6,7,8)) |> 
+  gather() |> 
+  na.omit() |> 
+  mutate(col = as_factor(c(1, 3, 3, 3, 3, 3, 3,3)),
+         key = fct_rev(as_factor(key)))
+
+p2_hypo <- hypo_alt_tbl |> 
+  ggplot(aes(x = value, y = key, fill = col)) +
+  theme_minimal() +
+  geom_point(shape = 21, size = 5, show.legend = FALSE) +
+  scale_fill_okabeito(order = c(1,3)) +
+  annotate("text", x = c(2, 3, 4, 5, 6, 7, 8)+0.2, y = 1-0.15, 
+           label = c(expression(c[1]), expression(c[2]), expression(c[3]),
+                     expression(c[4]), expression(c[5]), expression(c[6]),
+                     expression(c[7]))) +
+  annotate("text", x = c(1)+0.2, y = 2-0.15, 
+           label = expression(d[1])) +
+  annotate("label", x = 6.1, y = 2, hjust = "left",
+           label = expression(Pr~(Dog<Cat)~"="~frac(0, 7)~"≈"~0))+
+  labs(x = "Sprungweite in [cm]", y = "") +
+  theme(axis.title.y = element_text(size = 14, face = 2),
+        axis.text.y = element_text(size = 12), 
+        axis.title.x = element_text(size = 14, face = 2),
+        axis.text.x = element_blank(),
+        plot.title = element_text(size = 17),
+        plot.subtitle = element_text(size = 12, face = "italic"),
+        plot.caption = element_text(size = 12),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14, face = 2),
+        legend.position = "none")
