a1399d7fadd80291f4d5acf4bf01eaa0dd6d807c
Jochen Kruppa <jkruppa@users.noreply.github.com>
2025-08-20 20:14:43 +0200

    added

--- images/R/eda-generation.R
+++ images/R/eda-generation.R
@@ -223,3 +223,197 @@ p4_square_cov <- tibble(x = c(0.5, 1.5, 2, 2.5, 3),
   labs(x = "Kovariate", y = "Messwert (y)",
        title = "Effekt und Streuung",
        subtitle = "")
+
+
+p4_square_fac <- tibble(f = c(rep(1, 6), rep(2, 6)),
+                        y = c(4, 5, 5.5, 6.5, 7, 8,  
+                              7, 8, 8.5, 9.5, 10, 11)) |> 
+  ggplot(aes(f, y)) +
+  theme_minimal() +
+  geom_smooth(method = "lm", formula = y ~ x, color = "#CC79A7", linewidth = 1,
+              fullrange = TRUE, se = TRUE, fill = "#CC79A7", alpha = 0.2) +
+  geom_point(color = "gray50", alpha = 0.5, size = 3) +
+  stat_summary(fun.data=mean_sdl, , fun.args = list(mult = 1), 
+               geom="pointrange", shape = 23, 
+               fill = c("#56B4E9", "#E69F00"), size = 0.75) +
+  annotate("label", x = c(1, 2), y = 12, label = c(expression(n[1]~"="~6), 
+                                                   expression(n[2]~"="~6))) +
+  annotate("label", x = 1.5, y = 7.5, label = expression(atop(bold(Delta[abs]~"="~+"3.0"),
+                                                              bold(Delta[rel]~"="%*%"1.5"))),
+           color = "#CC79A7", size = 5) +
+  scale_y_continuous(limits = c(3, 12.5), breaks = c(6, 9)) +
+  scale_x_continuous(limits = c(0.5, 2.5),breaks = c(1, 2), label = c("A.1", "A.2")) +
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "none",
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 12, face = "bold"),
+        plot.subtitle = element_text(size = 12, face = "italic"),
+        title = element_text(size = 14, face = "bold")) +
+  labs(x = "Faktor A", y = "Messwert (y)",
+       title = "Effekt und Streuung",
+       subtitle = "") 
+
+p1_square_fac <- tibble(f = c(rep(1, 6), rep(2, 6)),
+                        y = c(7.499, 7.5, 7.5, 7.5, 7.5, 7.501,  
+                              7.499, 7.5, 7.5, 7.5, 7.5, 7.501)) |> 
+  ggplot(aes(f, y)) +
+  theme_minimal() +
+  geom_smooth(method = "lm", formula = y ~ x, color = "#CC79A7", linewidth = 1,
+              fullrange = TRUE, se = TRUE, fill = "#CC79A7", alpha = 0.2) +
+  geom_point(color = "gray50", alpha = 0.5, size = 3) +
+  stat_summary(fun.data=mean_sdl, , fun.args = list(mult = 1), 
+               geom="pointrange", shape = 23, 
+               fill = c("#56B4E9", "#E69F00"), size = 0.75) +
+  annotate("label", x = c(1, 2), y = 12, label = c(expression(n[1]~"="~6), 
+                                                   expression(n[2]~"="~6))) +
+  annotate("label", x = 1.5, y = 7.5, label = expression(atop(bold(Delta[abs]~"="~+"0.0"),
+                                                              bold(Delta[rel]~"="%*%"1.0"))),
+           color = "#CC79A7", size = 5) +
+  scale_y_continuous(limits = c(3, 12.5), breaks = c(6, 9)) +
+  scale_x_continuous(limits = c(0.5, 2.5),breaks = c(1, 2), label = c("A.1", "A.2")) +
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "none",
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 12, face = "bold"),
+        plot.subtitle = element_text(size = 12, face = "italic"),
+        title = element_text(size = 14, face = "bold")) +
+  labs(x = "Faktor A", y = "Messwert (y)",
+       title = "Kein Effekt und keine Streuung",
+       subtitle = "")
+
+
+p2_square_fac <- tibble(f = c(rep(1, 6), rep(2, 6)),
+                        y = c(5.999, 6, 6, 6, 6, 6.001,  
+                              8.999, 9, 9, 9, 9, 9.001)) |> 
+  ggplot(aes(f, y)) +
+  theme_minimal() +
+  geom_smooth(method = "lm", formula = y ~ x, color = "#CC79A7", linewidth = 1,
+              fullrange = TRUE, se = TRUE, fill = "#CC79A7", alpha = 0.2) +
+  geom_point(color = "gray50", alpha = 0.5, size = 3) +
+  stat_summary(fun.data=mean_sdl, , fun.args = list(mult = 1), 
+               geom="pointrange", shape = 23, 
+               fill = c("#56B4E9", "#E69F00"), size = 0.75) +
+  annotate("label", x = c(1, 2), y = 12, label = c(expression(n[1]~"="~6), 
+                                                   expression(n[2]~"="~6))) +
+  annotate("label", x = 1.5, y = 7.5, label = expression(atop(bold(Delta[abs]~"="~+"3.0"),
+                                                              bold(Delta[rel]~"="%*%"1.5"))),
+           color = "#CC79A7", size = 5) +
+  scale_y_continuous(limits = c(3, 12.5), breaks = c(6, 9)) +
+  scale_x_continuous(limits = c(0.5, 2.5),breaks = c(1, 2), label = c("A.1", "A.2")) +
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "none",
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 12, face = "bold"),
+        plot.subtitle = element_text(size = 12, face = "italic"),
+        title = element_text(size = 14, face = "bold")) +
+  labs(x = "Faktor A", y = "Messwert (y)",
+       title = "Effekt und keine Streuung",
+       subtitle = "")
+
+p3_square_fac <- tibble(f = c(rep(1, 6), rep(2, 6)),
+                        y = c(5, 6, 7, 8, 9, 10,  
+                              5, 6, 7, 8, 9, 10)) |> 
+  ggplot(aes(f, y)) +
+  theme_minimal() +
+  geom_smooth(method = "lm", formula = y ~ x, color = "#CC79A7", linewidth = 1,
+              fullrange = TRUE, se = TRUE, fill = "#CC79A7", alpha = 0.2) +
+  geom_point(color = "gray50", alpha = 0.5, size = 3) +
+  stat_summary(fun.data=mean_sdl, , fun.args = list(mult = 1), 
+               geom="pointrange", shape = 23, 
+               fill = c("#56B4E9", "#E69F00"), size = 0.75) +
+  annotate("label", x = c(1, 2), y = 12, label = c(expression(n[1]~"="~6), 
+                                                   expression(n[2]~"="~6))) +
+  annotate("label", x = 1.5, y = 7.5, label = expression(atop(bold(Delta[abs]~"="~+"0.0"),
+                                                              bold(Delta[rel]~"="%*%"1.0"))),
+           color = "#CC79A7", size = 5) +
+  scale_y_continuous(limits = c(3, 12.5), breaks = c(6, 9)) +
+  scale_x_continuous(limits = c(0.5, 2.5),breaks = c(1, 2), label = c("A.1", "A.2")) +
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "none",
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 12, face = "bold"),
+        plot.subtitle = element_text(size = 12, face = "italic"),
+        title = element_text(size = 14, face = "bold")) +
+  labs(x = "Faktor A", y = "Messwert (y)",
+       title = "Kein Effekt und Streuung",
+       subtitle = "")
+
+
+p_venn_gen <- ggplot() +
+  theme_void() +
+  geom_circle(aes(x0 = 1, y0 = 1, r = 1), 
+              fill = "#56B4E9", alpha = 0.5) +
+  geom_circle(aes(x0 = (1+2.25)/2, y0 = 0, r = 1),
+              fill = "#009E73", alpha = 0.5) +
+  geom_circle(aes(x0 = 2.25, y0 = 1, r = 1), 
+              fill = "#D55E00", alpha = 0.4) +
+  annotate("text", x = (1+2.25)/2, y = 0.7, label = "Daten", size = 9, fontface = 2) +
+  annotate("text", x = 0.6, y = 1.6, label = "Effekt", size = 9, fontface = 2) +
+  annotate("text", x = 0.6, y = 1.1, label = "Wie groß ist\nder Unterschied?", 
+           size = 6, fontface = 3) +
+  annotate("text", x = 2.5, y = 1.6, label = "Streuung", size = 9, fontface = 2) +
+  annotate("text", x = 2.6, y = 1.1, label = "Wie unterschiedlich\nsind die generierten\nMesswerte?", size = 6, fontface = 3) +
+  annotate("text", x = (1+2.25)/2, y = -0.75, label = "Fallzahl", size = 9, fontface = 2) +
+  annotate("text", x = (1+2.25)/2, y = -0.25, label = "Wie viele\nBeobachtungen sollen\ngeneriert werden?", size = 6, fontface = 3) +
+  labs(x = "", y = "", fill = "",
+       title = "Dreiklang der Generierung von Daten",
+       subtitle = "Zusammenhang von Effekt, Streuung und Fallzahl") +
+  theme(plot.title = element_text(size = 16, face = "bold"),
+        plot.subtitle = element_text(size = 12, face = "italic")) 
+
+p_theo_observed <- ggplot(data.frame(x = c(-3.25, 3.25)), aes(x)) +
+  theme_minimal() +
+  stat_function(fun = dnorm, linewidth = 1, args = list(mean = 0, sd = 2), 
+                xlim = c(-8.25, 8.25), color = "#E89F00") +
+  stat_function(fun = dnorm, xlim = c(-8.25, 8.25), args = list(mean = 0, sd = 2), 
+                geom = "area", fill = "#E89F00", alpha = 0.25) +
+  geom_segment(data = tibble(x = c(1.3, 2.1, 3.7, 4.2, 5.7), 
+                             xend = c(1.3, 2.1, 3.7, 4.2, 5.7),
+                             y = rep(-0.1, 5), yend = rep(-0.175, 5)), 
+               aes(x, xend = xend, y, yend = yend), color = "#009E73",
+               linewidth = 0.75, linetype = 1) +
+  annotate("point", x = mean(c(1.3, 2.1, 3.7, 4.2, 5.7)), y = (-0.1 + -0.175)/2, 
+           shape = 23, fill = "#CC79A7", size = 2.5) +
+  geom_segment(data = tibble(x = c(-5.3, -4.6, -2.1, -2.5, 1.1), 
+                             xend = c(-5.3, -4.6, -2.1, -2.5, 1.1),
+                             y = rep(-0.3, 5), yend = rep(-0.375, 5)), 
+               aes(x, xend = xend, y, yend = yend), color = "#009E73",
+               linewidth = 0.75, linetype = 1) +
+  annotate("point", x = mean(c(-5.3, -4.6, -2.1, -2.5, 1.1)), y = (-0.3 + -0.375)/2, 
+           shape = 23, fill = "#CC79A7", size = 2.5) +
+  geom_segment(data = tibble(x = c(-0.3, -1.6, -0.1, 2.5, 3.1), 
+                             xend = c(-0.3, -1.6, -0.1, 2.5, 3.1),
+                             y = rep(-0.5, 5), yend = rep(-0.575, 5)), 
+               aes(x, xend = xend, y, yend = yend), color = "#009E73",
+               linewidth = 0.75, linetype = 1) +
+  annotate("point", x = mean(c(-0.3, -1.6, -0.1, 2.5, 3.1)), y = (-0.5 + -0.575)/2, 
+           shape = 23, fill = "#CC79A7", size = 2.5) +
+  geom_segment(data = tibble(x = c(2.3, 3.7, -1.0, 2.9, -2.81), 
+                             xend = c(2.3, 3.7, -1.0, 2.9, -2.81),
+                             y = rep(-0.7, 5), yend = rep(-0.775, 5)), 
+               aes(x, xend = xend, y, yend = yend), color = "#009E73",
+               linewidth = 0.75, linetype = 1) +
+  annotate("point", x = mean(c(2.3, 3.7, -1.0, 2.9, -2.81)), y = (-0.7 + -0.775)/2, 
+           shape = 23, fill = "#CC79A7", size = 2.5) +
+  geom_segment(data = tibble(x = c(0.9, 2.8, 0.2, 2.9, 2.0), 
+                             xend = c(0.9, 2.8, 0.2, 2.9, 2.0),
+                             y = rep(-0.9, 5), yend = rep(-0.975, 5)), 
+               aes(x, xend = xend, y, yend = yend), color = "#009E73",
+               linewidth = 0.75, linetype = 1) +
+  annotate("point", x = mean(c(0.9, 2.8, 0.2, 2.9, 2.0)), y = (-0.9 + -0.975)/2, 
+           shape = 23, fill = "#CC79A7", size = 2.5) +
+  geom_vline(xintercept = c(0)) + 
+  geom_hline(yintercept = c(0)) + 
+  scale_y_continuous(breaks = c((-0.1 + -0.175)/2, (-0.3 + -0.375)/2,
+                                (-0.5 + -0.575)/2, (-0.7 + -0.775)/2, (-0.9 + -0.975)/2)) +
+  scale_x_continuous(breaks = c(-4, -2, 0, 2, 4), limits = c(-6, 6)) +
+  theme(panel.grid.minor = element_blank(),
+        #panel.grid.major.y = element_blank(), 
+        axis.text.x = element_text(size = 12),
+        axis.text.y = element_blank(),
+        axis.title = element_text(size = 12, face = "bold"),
+        plot.subtitle = element_text(size = 12, face = "italic"),
+        title = element_text(size = 14, face = "bold")) +
+  labs(x = "", y = "",
+       title = "Theoretische Verteilung vs. beobachtete Werte",
+       subtitle = "Bei einer kleinen Stichprobe weicht der beobachtete vom theoretischen Mittelwert ab") 
